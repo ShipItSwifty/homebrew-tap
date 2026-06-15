1abd8f82d364a990243059b46f57a1cba39e79d1
maniramezan <mani.ramezan@gmail.com>
2026-06-15 14:19:25 -0400

    Fix formula check: remove redundant version, fix shellcheck warnings

--- Formula/shipit.rb
+++ Formula/shipit.rb
@@ -6,8 +6,6 @@ class Shipit < Formula
   head "https://github.com/ShipItSwifty/shipitswifty.git", branch: "main"
 
   stable do
-    version "0.2.1"
-
     on_macos do
       url "https://github.com/ShipItSwifty/shipitswifty/releases/download/0.2.1/shipit-0.2.1-macos-universal.tar.gz"
       sha256 "7a5900e71aca8681f02328e8df4e86692c2b1b96c76bb552e1e95b13ec7cab72"
